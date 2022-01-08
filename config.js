13adfb907c0b1febf58104fbd557c8869377e3c6
shanekizito <shaneyoungk@gmail.com>
2022-01-08 21:20:40 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports= {
+  consumer_key:'RmcRgbjTCuCiblLfTm3gudtXd',
+  consumer_secret:'Ij2HJUwZ9NbfoXY7NQHCCDfFMX64uZ9nTHuThLtgPmBxOf34yW',
+  access_token:'1479854775713861637-SOv9LBJKSbgqxV8x8oxwRH6GjwPmur',
+  access_token_secret:'tcwN7cfMhMcBlTY6o5OaPL7eI8nAGv2Prkh4zsVjM6mNb'
+}
